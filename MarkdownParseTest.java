e9e849b28050ac8deb2e205fdf57f4759865d7e8
Jonathan Duong <jonaduong123@gmail.com>
2022-01-26 15:30:34 -0800

    getLinks() Test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,24 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void getLinks() throws IOException {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-page.html");
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(expected, MarkdownParse.getLinks(contents));
     }
 }
